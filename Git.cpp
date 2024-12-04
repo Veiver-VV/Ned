33410b4143aa8ac697be5e5d289d278883a00b8c
veiver-VV <otaremir123@gmail.com>
2024-12-04 09:28:49 +0500

    proverka

---
+++ Git.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cctype> 
+#include <string>
+
+int main() {
+    std::string symbol;
+    std::cout << "16 tañbaly san engiziñiz: ";
+    std::cin >> symbol;
+
+
+    if (symbol.length() == 16) {
+        bool is_valid = true;
+        for (char c : symbol) {
+            if (!isdigit(c)) {
+                is_valid = false;
+                break;
+            }
+        }
+
+        if (is_valid) {
+            std::cout << "Engizilgen san 16 tañbaly san bolyp tabylady.\n";
+        }
+        else {
+            std::cout << "Engizilgen san san emes.\n";
+        }
+    }
+    else {
+        std::cout << "Engizilgen san 16 tañbaly san emes.\n";
+    }
+
+    return 0;
+}
